81e5809b532fa5d809614145eb0ce2e2d170a081
popeye <popeye0618@gmail.com>
2026-05-15 03:46:06 +0900

    ƒeat: 프로젝트 초기 설정

---
+++ UniTT/UniTTApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniTTApp.swift
+//  UniTT
+//
+//  Created by 천승환 on 5/15/26.
+//
+
+import SwiftUI
+
+@main
+struct UniTTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UniTTTests/UniTTTests.swift
@@ -0,0 +1,17 @@
+//
+//  UniTTTests.swift
+//  UniTTTests
+//
+//  Created by 천승환 on 5/15/26.
+//
+
+import Testing
+@testable import UniTT
+
+struct UniTTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
